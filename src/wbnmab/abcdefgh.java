01e58da1c5f0000c99748528df5da8662d230000
mnetke.dev <mnetke.dev@DESKTOP-0BP7HQF>
2022-12-18 08:15:58 +0530

    [SN]:1 Folder created

---
+++ src/wbnmab/abcdefgh.java
@@ -0,0 +1,13 @@
+package wbnmab;
+
+public class abcdefgh
+{
+	public static void main(String[] args) 
+	{
+	System.out.println("ss");
+	System.out.println("sd");
+	System.out.println("sdndfg");
+	System.out.println("abcde");
+	}
+
+}
