222ec6f1e952eaa4611569c31bb049f047eb89fd
mnetke.dev <mnetke.dev@DESKTOP-0BP7HQF>
2022-12-18 09:25:47 +0530

    [SN): remove

--- src/wbnmab/abcdefgh.java
+++ src/wbnmab/abcdefgh.java
@@ -5,7 +5,6 @@ public class abcdefgh
 	public static void main(String[] args) 
 	{
 	System.out.println("ss");
-	System.out.println("sd");
 	System.out.println("sdndfg");
 	System.out.println("abcde");
 	}
